1bdf4c3ee56826783dd0c7f35d44aa1bdfb8bf3f
ertoth <ertoth@paypal.com>
2014-03-11 10:08:38 -0700

    Add implementation and test cases.

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var path2regex = require('path-to-regexp');
+
+
+module.exports = function reverend(route, obj) {
+    var keys;
+
+    keys = [];
+    path2regex(route, keys);
+
+    keys.forEach(function (key) {
+        var value, regex;
+
+        value = obj[key.name];
+        regex = '\\:' + key.name;
+
+        if (key.optional) {
+            // Include the trailing '?' in the replacement.
+            regex += '\\?';
+            if (value === undefined) {
+                // No value so remove potential trailing '/'
+                // since the path segment is optional.
+                value = '';
+                regex += '\\/?';
+            }
+        } else if (value === undefined) {
+            value = '';
+        }
+
+        route = route.replace(new RegExp(regex, 'g'), value);
+    });
+
+    return route;
+}
---
+++ test/reverend.js
@@ -0,0 +1,111 @@
+'use strict';
+
+var test = require('tape');
+var reverend = require('../');
+
+
+test('noop', function (t) {
+    var path, actual;
+
+    path = '/';
+    actual = reverend(path, {});
+    t.equal(actual, path);
+
+    path = '/foo';
+    actual = reverend(path, {});
+    t.equal(actual, path);
+
+    path = '/foo/';
+    actual = reverend(path, {});
+    t.equal(actual, path);
+
+    path = '/foo/bar';
+    actual = reverend(path, {});
+    t.equal(actual, path);
+
+    path = '/foo/bar/';
+    actual = reverend(path, {});
+    t.equal(actual, path);
+
+    t.end();
+});
+
+
+test('replacement', function (t) {
+    var data, path, actual;
+
+    data = {
+        foo: 'foo',
+        baz: 'baz'
+    };
+
+    path = '/:foo';
+    actual = reverend(path, data);
+    t.equal(actual, '/foo');
+
+    path = '/:foo/';
+    actual = reverend(path, data);
+    t.equal(actual, '/foo/');
+
+    path = '/:foo/bar';
+    actual = reverend(path, data);
+    t.equal(actual, '/foo/bar');
+
+    path = '/:foo/bar/';
+    actual = reverend(path, data);
+    t.equal(actual, '/foo/bar/');
+
+    path = '/:foo/bar/:baz';
+    actual = reverend(path, data);
+    t.equal(actual, '/foo/bar/baz');
+
+    path = '/:foo/bar/:baz/';
+    actual = reverend(path, data);
+    t.equal(actual, '/foo/bar/baz/');
+
+    path = '/:foo/bar/:foo/';
+    actual = reverend(path, data);
+    t.equal(actual, '/foo/bar/foo/');
+
+    path = '/:foo/:bar/:foo/';
+    actual = reverend(path, data);
+    t.equal(actual, '/foo//foo/');
+
+    t.end();
+});
+
+
+test('optional replacement', function (t) {
+    var data, path, actual;
+
+    data = {
+        foo: 'foo',
+        baz: 'baz'
+    };
+
+    path = '/:foo?';
+    actual = reverend(path, data);
+    t.equal(actual, '/foo');
+
+    path = '/:foo?/';
+    actual = reverend(path, data);
+    t.equal(actual, '/foo/');
+
+    path = '/:bar?';
+    actual = reverend(path, data);
+    t.equal(actual, '/');
+
+    path = '/:bar?/';
+    actual = reverend(path, data);
+    t.equal(actual, '/');
+
+    path = '/:bar?/:baz?';
+    actual = reverend(path, data);
+    t.equal(actual, '/baz');
+
+    path = '/:bar?/:baz?/';
+    actual = reverend(path, data);
+    t.equal(actual, '/baz/');
+
+    t.end();
+});
